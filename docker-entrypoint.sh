6a9bd08b26738e50144eda52261a82625c075cf1
Aboubakry Dieng <diengabzo@gmail.com>
2025-10-30 13:02:40 +0000

    debug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if command -v psql > /dev/null; then
     attempts=0
     max_attempts=30  # 30 * 2 = 60 secondes max
-    until PGPASSWORD="${DB_PASSWORD}" psql "postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?sslmode=${DB_SSLMODE}" -c "SELECT 1;" > /dev/null 2>&1 || [ $attempts -ge $max_attempts ]; do
+    until PGPASSWORD="${DB_PASSWORD}" psql "postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?sslmode=${DB_SSLMODE:-require}" -c "SELECT 1;" > /dev/null 2>&1 || [ $attempts -ge $max_attempts ]; do
         echo "Database is unavailable - sleeping (attempt $((attempts+1))/$max_attempts)"
         sleep 2
         attempts=$((attempts+1))
